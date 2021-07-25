531fc68fb0b376368f5db58733f5935e35b8c638
bipluv89 <biplab.das8109@gmail.com>
2021-07-25 23:45:06 +1000

    test3

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -5,10 +5,10 @@ const NavBar = (props) => (
   <nav className="column is-2 menu">
     <p className="menu-label">Menu</p>
     <ul className="menu-list">
-      <NavLink to="/products1" activeClassName="active-link">
+      <NavLink to="/products" activeClassName="active-link">
         Products
       </NavLink>
-      <NavLink to="/about12" activeClassName="active-link">
+      <NavLink to="/about" activeClassName="active-link">
         About
       </NavLink>
     </ul>
